e18e1cf6800f9e370a3eda51d6b4bf5d40e8dece
Rikuto13ten <rikuto1367@gmail.com>
2025-05-20 10:39:01 +0900

    update

--- keybind.el
+++ keybind.el
@@ -1,10 +1,13 @@
+;; SPC 起点としたキーバインド
 (evil-leader/set-key
   ;; ファイル操作
   "fe" 'neotree-change-root
   "e" 'neotree-toggle
 
   ;; ウィンドウ間の移動（hjkl）
-  "h" 'evil-window-left         ;; SPC w h で左のウィンドウに移動
-  "j" 'evil-window-down         ;; SPC w j で下のウィンドウに移動
-  "k" 'evil-window-up           ;; SPC w k で上のウィンドウに移動
-  "l" 'evil-window-right)    ;; SPC w o で他のウィンドウをすべて閉じる
+  "h" 'evil-window-left  ;; SPC h で左のウィンドウに移動
+  "j" 'evil-window-down ;; SPC j で下のウィンドウに移動
+  "k" 'evil-window-up ;; SPC k で上のウィンドウに移動
+  "l" 'evil-window-right
+  "x" 'execute-extended-command ;; コマンド実行
+  )
