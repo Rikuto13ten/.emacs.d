67459a36998b79ffb1fc01487663a6ea3bd908cd
Rikuto Noda <rikuto13ten@RikutonoMacBook-Pro.local>
2025-05-28 22:10:02 +0900

    add magit

--- package.el
+++ package.el
@@ -126,3 +126,15 @@
   (setq dirvish-mode-line-format
         '(:left (sort symlink) :right (omit yank index))))
 
+(use-package magit
+  :ensure t
+  :bind (("C-x g" . magit-status)
+         ("C-x M-g" . magit-dispatch-popup))
+  :config
+  (defun mu-magit-kill-buffers ()
+    "Restore window configuration and kill all Magit buffers."
+    (interactive)
+    (let ((buffers (magit-mode-get-buffers)))
+      (magit-restore-window-configuration)
+      (mapc #'kill-buffer buffers)))
+  (bind-key "q" #'mu-magit-kill-buffers magit-status-mode-map))
