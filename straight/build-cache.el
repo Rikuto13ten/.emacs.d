54fa0b33eb929c5112ab5848703238e970d86fc7
rikuto1367@gmail.com <Rikuto13ten>
2025-08-12 22:10:00 +0900

    Auto-sync 2025-08-12 22:10:00

---
+++ straight/build-cache.el
@@ -0,0 +1,14 @@
+
+:tanat
+
+"29.4"
+
+#s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8125 data ("straight" ("2025-08-12 22:09:58" nil (:type git :host github :repo "radian-software/straight.el" :branch "master" :package "straight" :local-repo "straight.el"))))
+
+#s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8125 data ())
+
+#s(hash-table size 65 test eq rehash-size 1.5 rehash-threshold 0.8125 data ())
+
+("straight")
+
+t
